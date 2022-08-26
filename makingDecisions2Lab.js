bfd57df67298386d31502597f136de6c56992927
Keith Holdaway <keithholdaway1@hotmail.com>
2022-08-25 19:20:56 -0700

    problem 5 fin

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -52,7 +52,7 @@ for (i = 0; i < nums.length; i++){
   }
 }
 
-console.log(evensArr);
+
 ////////// PROBLEM 5 //////////
 
 // Do not edit the code below.
@@ -64,7 +64,17 @@ var score = 74
 */
 
 //Code Here
-
+if (score >= 90){
+  console.log("A");
+} else if (score <= 89 && score >= 80){
+  console.log(`B`);
+} else if (score <= 79 && score >= 70){
+  console.log(`C`);
+} else if (score <= 69 && score >= 60){
+  console.log(`D`);
+} else if (score < 60){
+  console.log(`F`);
+}
 
 ////////// Intermediate Problems //////////
 
