35c125b8ae76e6b0b587c85ed86501073281dac8
Keith Holdaway <keithholdaway1@hotmail.com>
2022-08-25 19:41:27 -0700

    Problem 9 fin

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -89,7 +89,7 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 */
 
 //Code Here
-
+let someNum = myFavoriteNumbers.slice(4, 5);
 
 ////////// PROBLEM 7 //////////
 
@@ -97,8 +97,13 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 // Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
 //Code Here
+if (myFavoriteNumbers.length > 7){
+  console.log(`There are not enough elements in this array,`);
+} else if (myFavoriteNumbers.length === 7){
+  someNum = myFavoriteNumbers.splice(6, 7);
+}
 
-
+console.log(someNum);
 ////////// PROBLEM 8 //////////
 
 // Do not edit the code below.
@@ -120,7 +125,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+for (i = 4; i >= 0; i--){
+  console.log(letters[i]);
+}
 
 ////////// Advanced Problems //////////
 
